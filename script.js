723bf7eaf756fdfe9e00a25e06d00ba628e8983c
elenagurchinskaia <136741210+elenagurchinskaia@users.noreply.github.com>
2023-06-29 13:59:49 -0500

    added html, java script, and did a starter code

---
+++ script.js
@@ -0,0 +1,17 @@
+// To generate a series of questions we need to create a variety of characters
+
+var rock = "R";
+var paper = "P";
+var scissor = "S";
+
+
+// // Generate a function window to run the game
+// function window.prompt() {
+//     alert("");
+// }
+
+sign = window.prompt("Choose R P S?");
+
+if ()
+
+// sign = prompt("Choose R P S?");
